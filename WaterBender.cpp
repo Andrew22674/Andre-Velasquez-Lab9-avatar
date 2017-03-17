8c7a4ea6023e306a9798f7f6a993eb7630a85704
Andre V <avela@unitec.edu>
2017-03-17 17:47:00 -0600

    Modificando clases

--- WaterBender.cpp
+++ WaterBender.cpp
@@ -9,7 +9,32 @@ WaterBender::WaterBender(double defensa, double ofensa, double fuerza, double su
 }
 
 void WaterBender::Ataque(Bender* bender){
-  bender -> setDefensa((bender -> getDefensa()) - ofensa);
+  srand (time(NULL));
+  double atk = ofensa;
+  if(suerte >= 0 && suerte <= 5){
+    int ran = rand() % 1000 + 1;
+
+    if(ran == 50){
+      cout << "Smash hit!" << endl;
+      atk += atk * 0.5;
+    }
+  }else if(suerte >= 6 && suerte <= 9){
+    int ran = rand() % 100 + 1;
+
+    if(ran % 5 == 0){
+      cout << "Smash hit!" << endl;
+      atk += atk * 0.5;
+    }
+  }else{
+    int ran = rand() % 10 + 1;
+    if(ran % 3 == 0){
+      cout << "Smash hit!" << endl;
+      atk += atk * 0.5;
+    }
+  }
+
+
+  bender -> setDefensa((bender -> getDefensa()) - atk);
 }
 
 void WaterBender::AtaqueEspecial(Bender* bender){
